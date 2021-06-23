17a0c017e203bc5f610fbf806dd9671341d62f1c
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2021-06-23 18:00:43 -0300

    teste

---
+++ leitor.js
@@ -0,0 +1,23 @@
+var voices = speechSynthesis.getVoices();
+const voice = voices.filter(voice => voice.lang.includes("BR"))[0]
+
+function adiconaLeitor() {
+    const paragrafos = document.querySelectorAll("p.leitor")
+    paragrafos.forEach(p => {
+        const paragrafo = {
+            text: p.innerText,
+            play: function() {
+                const utterance = new SpeechSynthesisUtterance(this.text)
+                utterance.voice = voice
+                speechSynthesis.speak(utterance)
+            }
+        }
+
+        const button = document.createElement("button")
+        button.innerText = "play"
+        button.addEventListener("click", () => { paragrafo.play() })
+        p.appendChild(button)
+    })
+}
+
+adiconaLeitor()
